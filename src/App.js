a911a493e63aa7cf75b35b2e42bc10510ce8de0a
ClaudiaVargasSilva <75755572+ClaudiaVargasSilva@users.noreply.github.com>
2021-01-19 14:25:18 -0300

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import { extend } from 'jquery';
 import './App.css';
+import React from 'react'
+//import Estruc from './component/estruc';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isColorSelect: " "
+    };
+  }
+
+  render() {
+    let red2 = " ";
+    let yellow2 = " ";
+    let green2 = " ";
+
+    if (this.state.isColorSelect == "red")
+      red2 = "gloss";
+    if (this.state.isColorSelect == "yellow")
+      yellow2 = "gloss";
+    if (this.state.isColorSelect == "green")
+      green2 = "gloss";
+
+    return (
+      <body>
+        <div className="contenedor">
+          <div onClick={() => this.setState({ isColorSelect: "red" })} className={"red " + red2} ></div>
+          <div onClick={() => this.setState({ isColorSelect: "yellow" })} className={"yellow " + yellow2} ></div>
+          <div onClick={() => this.setState({ isColorSelect: "green" })} className={"green " + green2} ></div>
+        </div>
+      </body>
+    )
+  }
+};
+
 
 export default App;
+/**/
